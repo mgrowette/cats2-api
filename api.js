761b6be2ccaae126ebda95251f9a96b779ab7f66
migrow <michael.growette@gmail.com>
2018-02-07 15:36:53 -0500

    created home route

--- api.js
+++ api.js
@@ -1,1 +1,12 @@
+require('dotenv').config()
+const { propOr } = require('ramda')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = propOr(9999, 'PORT', process.env)
 
+app.get('/', (req, res) =>
+  res.send(`<h1> Welcome to the world of cool cats!</h1>`)
+)
+
+app.listen(port, () => console.log('CATS up and running on port', port))
